8a4bb005ea57baa6684df946a790233357f54585
X!v3r <vanniealdamar@gmail.com>
2023-05-29 08:11:35 +0800

    Update Burp.sh

--- burp.sh
+++ burp.sh
@@ -7,11 +7,10 @@
 
   # Execute Burp Suite Professional with Keyloader
     echo 'Executing Burp Suite Professional with Keyloader'
-    echo "java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v2022.8.5.jar &" > burp1
-    chmod +x burp1
-    cp burp1 /bin/burp1 
-    (./burp1)
+    echo "java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v2022.8.5.jar &" > Burp
+    chmod +x Burp
+    cp Burp /bin/Burp 
+    (./Burp)
 else
     echo "Execute Command as Root User"
     exit
-fi
