4aa058475b5cc263c332b4aeb8e203519f7798c5
X!v3r <vanniealdamar@gmail.com>
2023-05-29 08:42:01 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-    # Download Burp Suite Profesional Latet Version
+  # Download Burp Suite Profesional Latest Version
     echo 'Downloading Burp Suite Professional v2022.8.5 ....'
     Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.8.5&type=Jar"
     wget "$Link" -O burpsuite_pro_v2022.8.5.jar --quiet --show-progress
     sleep 2s
 
-    # execute Keygenerator
+  # Execute Key Generator
     echo 'Starting Keygenerator'
     (java -jar keygen.jar) &
     sleep 3s
     
-    # Execute Burp Suite Professional with Keyloader
+  # Execute Burp Suite Professional with Keyloader
     echo 'Executing Burp Suite Professional with Keyloader'
-    echo "java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:/loader.jar -noverify -jar /burpsuite_pro_v2022.8.5.jar &" > Burp
+    echo "java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v2022.8.5.jar &" > Burp
     chmod +x Burp
     cp Burp /bin/Burp 
     (./Burp)
 else
-    echo "Run as Root"
-    exit
+    echo "Execute Command as Root User"
+exit
