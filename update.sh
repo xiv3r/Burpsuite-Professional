78ea41b9c30af7b6e5625de15be4c5d99ab79619
x!v3r <117867334+xiv3r@users.noreply.github.com>
2024-09-24 19:55:31 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,19 +2,11 @@
 
    # Remove old files
    echo 'Removing Old Files...'
-   rm -rf /usr/share/burpsuitepro/*.jar
-   rm -rf /usr/share/burpsuitepro/burpsuitepro
-   rm -rf /bin/burpsuitepro
-   
+   sudo rm -rf /bin/burpsuitepro
    sleep 1s &
-   # Copy loader.jar
-   cd /home/*/Burpsuite-Professional
-   cp loader.jar /usr/share/burpsuitepro
    
-   sleep 1s &
    # Download Burpsuitepro
    echo 'Downloading Burpsuite Professional'
-   cd /usr/share/burpsuitepro
    html=$(curl -s https://portswigger.net/burp/releases)
    version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
    Link="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=&"
@@ -26,5 +18,5 @@
     echo 'Executing Burpsuite Professional'
     echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
     chmod +x burpsuitepro
-    cp burpsuitepro /bin/burpsuitepro
+    sudo cp -r burpsuitepro /bin/burpsuitepro
     (./burpsuitepro)
