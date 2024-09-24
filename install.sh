178a4382bfda2f04b0a7cfc14ab6a78c78270e00
x!v3r <117867334+xiv3r@users.noreply.github.com>
2024-09-24 19:26:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,5 +28,5 @@
     echo 'Executing Burpsuite Professional with Key Generator'
     echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
     chmod +x burpsuitepro
-    cp burpsuitepro /bin/burpsuitepro
+    sudo cp -r burpsuitepro /bin/burpsuitepro
     (./burpsuitepro)
