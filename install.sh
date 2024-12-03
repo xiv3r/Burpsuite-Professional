cc86c06e5652b0432337e93a1b8f9ae3867bcd7e
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-03 15:44:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Installing Dependencies
-echo 'Installing Dependencies...'
- 
+echo "Installing Dependencies..."
 sudo apt install git wget openjdk-21-jre -y
 
 # Cloning
@@ -18,11 +17,11 @@ echo "Downloading Burpsuite Professional v$version ..."
 wget "$Link" -O burpsuite_pro_v$version.jar --quiet --show-progress
 
 # Execute Key Generator.
-echo 'Starting Key loader.jar...'
+echo "Starting Key loader.jar..."
 (java -jar loader.jar) &
 
 # Execute Burp Suite Professional
-echo 'Executing Burpsuite Professional...'
+echo "Executing Burpsuite Professional..."
 echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
 chmod +x burpsuitepro
 cp burpsuitepro /bin/burpsuitepro
