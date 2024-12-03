9c67de9be6f94a315b11b9044b23d7c94298905f
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-03 14:28:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@
     cd Burpsuite-Professional
     html=$(curl -s https://portswigger.net/burp/releases)
     version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
-    Link="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=&"
+    Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=&"
     echo $version
     wget "$Link" -O burpsuite_pro_v$version.jar --quiet --show-progress
 
