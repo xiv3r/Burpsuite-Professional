406e66817cfbaf46555a42e11431f274c9254471
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-14 07:39:01 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -30,10 +30,9 @@ if (!($jre8)){
 
 # Downloading Burp Suite Professional
     echo "`n`t`tDownloading Latest Burp Suite Professional..."
-	$version = "2024.11.1"
-	wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=Jar" -O "burpsuite_pro_v$version.jar"
+    $version = "2024.11.1"
+    Invoke-WebRequest -Uri "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=Jar" -       OutFile "burpsuite_pro_v$version.jar"
     echo "`nBurp Suite Professional is Downloaded.`n"
-}
 
 # Creating Burp.bat file with command for execution
 if (Test-Path burp.bat) {rm burp.bat}
